051b483a66b3d7ca75f0762e68cc2c330fa1cfb4
marcopolello <marco-pol@hotmail.it>
2020-12-02 19:26:11 +0100

    aggiunte serieTv con voto

--- js/script.js
+++ js/script.js
@@ -1,24 +1,36 @@
 var app = new Vue({
   el: '#app',
   data: {
-    url: 'https://api.themoviedb.org/3/search/movie/?api_key=',
+    urlmovie: 'https://api.themoviedb.org/3/search/movie/?api_key=',
+    urltv: 'https://api.themoviedb.org/3/search/tv/?api_key=',
     api_key: '295f81ea91a932d66a84c22c7c2f5ec6',
     listaFilm: [],
+    listaSerieTv: [],
     inputSearch: '',
   },
   methods:{
     // chiamata API
-    searchMovie: function () {
-      axios.get(this.url + this.api_key + '&language=it_IT&query=' + this.inputSearch)
-      .then(risposta => {
-        this.listaFilm = risposta.data.results;
-        //console.log(this.listaFilm);
+    search: function () {
+      const requestOne = axios.get(this.urlmovie + this.api_key + '&language=it_IT&query=' + this.inputSearch);
+      const requestTwo = axios.get(this.urltv + this.api_key + '&language=it_IT&query=' + this.inputSearch);
+      // richiesta API x film
+      requestOne.then(responseOne => {
+      this.listaFilm = responseOne.data.results;
+      })
+      // richiesta API x serietv
+      requestTwo.then(responseTwo => {
+      this.listaSerieTv = responseTwo.data.results;
       })
     },
-    // voto / 2 e arrotondato per eccesso
+    // votoFilm / 2 e arrotondato per eccesso
     filmVote: function (i) {
       let voto = this.listaFilm[i].vote_average;
       return Math.round(voto / 2);
-    }
+    },
+    //votoSerieTV
+    voteTv: function (i) {
+      let voto = this.listaSerieTv[i].vote_average;
+      return Math.round(voto / 2);
+    },
   }
 });
