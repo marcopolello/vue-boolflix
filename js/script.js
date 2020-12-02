b0df9dcad28c9687cceadcd0d1c2958c8ac74a90
marcopolello <marco-pol@hotmail.it>
2020-12-02 18:35:26 +0100

    funzione per voto da 1 a 5

--- js/script.js
+++ js/script.js
@@ -4,17 +4,21 @@ var app = new Vue({
     url: 'https://api.themoviedb.org/3/search/movie/?api_key=',
     api_key: '295f81ea91a932d66a84c22c7c2f5ec6',
     listaFilm: [],
-    ricercaInput: '',
-  },
-  mounted: function () {
-    axios.get(this.url + this.api_key + '&query=ritorno+al+futuro')
-    .then(risposta => {
-      //console.log(risposta);
-      this.listaFilm = risposta.data.results;
-      console.log(this.listaFilm);
-    });
+    inputSearch: '',
   },
   methods:{
-
+    // chiamata API
+    searchMovie: function () {
+      axios.get(this.url + this.api_key + '&language=it_IT&query=' + this.inputSearch)
+      .then(risposta => {
+        this.listaFilm = risposta.data.results;
+        //console.log(this.listaFilm);
+      })
+    },
+    // voto / 2 e arrotondato per eccesso
+    filmVote: function (i) {
+      let voto = this.listaFilm[i].vote_average;
+      return Math.round(voto / 2);
+    }
   }
 });
